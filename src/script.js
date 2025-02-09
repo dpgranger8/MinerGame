8db31fe7379d0cfe9b02282778211679320e5430
David Granger <dpgranger8@gmail.com>
2025-02-09 15:24:07 -0700

    finished wireframe of game

--- src/script.js
+++ src/script.js
@@ -1,12 +1,37 @@
 (function() {
     const grid = document.getElementById("minegrid");
+    const display = document.getElementById("display");
 
-    for (i = 0; i < 20; i++) {
+    for (let i = 0; i < 64; i++) {
 
-        const button = document.createElement("button");
-        button.classList.add("nodebutton");
-        button.textContent = "h"
+        let button = document.createElement("button");
+        button.classList.add("nodeButton");
+        button.id = i;
+        button.style.background = "linear-gradient(rgba(111, 111, 111, 0.2), rgba(111, 111, 111, 0.5)";
+        // button.textContent = 1;
+        let endAngle = 0;
+        let interval;
 
+        button.addEventListener("mouseover", function () {
+            if (!interval) {
+                interval = setInterval(() => {
+                    endAngle += 1;
+                    button.style.background = `conic-gradient(aquamarine 0deg, lime ${endAngle}deg, rgba(111, 111, 111, 0.5) ${endAngle}deg, rgba(111, 111, 111, 0.2) 360deg)`;
+                    if (endAngle >= 360) {
+                        endAngle = 0;
+                        let currentValue = parseInt(display.textContent);
+                        display.textContent = currentValue + 10;
+                    }
+                }, 10);
+            }
+        });
+
+        button.addEventListener("mouseleave", function () {
+            if (interval) {
+                clearInterval(interval);
+                interval = undefined;
+            }
+        })
         grid.appendChild(button);
     }
 })();
