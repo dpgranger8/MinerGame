cdedc133909c3ad6768397c69ba12a5c06f0bf6f
David Granger <dpgranger8@gmail.com>
2025-02-11 15:09:25 -0700

    added rewards table and rarity

--- src/script.js
+++ src/script.js
@@ -1,7 +1,18 @@
+let rewards = {
+    common: ["common", 1, ["aquamarine", "lime"], 50],
+    uncommon: ["uncommon", 5, ["aqua", "darkturquoise"], 25],
+    rare: ["rare", 10, ["hotpink", "mediumvioletred"], 15],
+    epic: ["epic", 50, ["lightCoral", "crimson"], 7],
+    legendary: ["legendary", 100, ["yellow", "gold"], 2.9],
+    mythic: ["mythic", 500, ["ghostwhite", "gainsboro"], 0.1]
+};
+
 (function() {
     const grid = document.getElementById("minegrid");
     const increaseHitbox = document.getElementById("extraPadding");
     const display = document.getElementById("display");
+
+    populateRewards();
 
     for (let i = 0; i < 64; i++) {
 
@@ -13,8 +24,6 @@
             gradientProgressedLight: "aquamarine",
             gradientProgressedDark: "lime"
         };
-
-        let bonus = Math.random
 
         let button = document.createElement("button");
         button.classList.add("nodeButton");
@@ -78,6 +87,56 @@ function bonusToast(button, bonus) {
     setTimeout(() => toast.remove(), 500);
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function capitalize(str) {
+    if (!str) {
+      return "";
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function populateRewards() {
+    const rewardsContainer = document.getElementById("rewards");
+    let newelement = document.createElement("h1");
+    newelement.textContent = "hello";
+
+    for (let key in rewards) {
+        let item = rewards[key];
+        let listItem = document.createElement("div");
+        listItem.classList.add("flex", "flex-row", "justify-between", "mb-2");
+
+        let itemGroupDiv = document.createElement("div");
+        itemGroupDiv.classList.add("flex", "basis-1/3", "self-center");
+
+        let nodeButton = document.createElement("button");
+        nodeButton.classList.add("nodeButton");
+        nodeButton.style.background = `linear-gradient(${item[2][0]}, ${item[2][1]}`;
+
+        let labelDiv = document.createElement("div");
+        labelDiv.classList.add("flex", "self-center", "ml-3");
+        labelDiv.textContent = capitalize(item[0]);
+
+        let chanceDiv = document.createElement("div");
+        chanceDiv.classList.add("flex", "justify-center",  "self-center", "ml-3");
+        chanceDiv.textContent = item[3] + "%";
+
+        let rewardDiv = document.createElement("div");
+        rewardDiv.classList.add("flex", "basis-1/3", "justify-end", "self-center", "ml-3");
+        rewardDiv.textContent = item[1];
+
+        let spacer = document.createElement("div");
+        spacer.classList.add("flex-grow");
+
+        rewardsContainer.appendChild(listItem);
+
+        itemGroupDiv.appendChild(nodeButton);
+        itemGroupDiv.appendChild(labelDiv);
+
+        listItem.appendChild(itemGroupDiv);
+        listItem.appendChild(chanceDiv);
+        listItem.appendChild(rewardDiv)
+    }
 }
